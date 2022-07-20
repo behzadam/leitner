cd17ae8ee674d3f000b1bec9af9ff71a8e4079ee
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-20 22:03:45 +0430

    feat: add flashcard entity

--- apps/api/src/flashcards/flashcards.module.ts
+++ apps/api/src/flashcards/flashcards.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FlashcardsService } from './flashcards.service';
 import { FlashcardsController } from './flashcards.controller';
+import { Flashcard } from './entities/flashcard.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Flashcard])],
   controllers: [FlashcardsController],
   providers: [FlashcardsService],
 })
