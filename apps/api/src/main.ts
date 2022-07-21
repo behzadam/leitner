adc30db18795b51827e04f6dd207ea9b663ad091
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-21 23:38:17 +0430

    chore: add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,11 +5,21 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Leitner example')
+    .setDescription('The Leitner API description')
+    .setVersion('1.0')
+    .addTag('Leitner')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
