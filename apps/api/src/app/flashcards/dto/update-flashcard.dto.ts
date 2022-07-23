e19c036615c45476c797bb41ca35f33949814320
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-23 18:06:59 +0430

    feat: add id property

--- apps/api/src/app/flashcards/dto/update-flashcard.dto.ts
+++ apps/api/src/app/flashcards/dto/update-flashcard.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateFlashcardDto } from './create-flashcard.dto';
 
-export class UpdateFlashcardDto extends PartialType(CreateFlashcardDto) {}
+export class UpdateFlashcardDto extends PartialType(CreateFlashcardDto) {
+  id: number;
+}
