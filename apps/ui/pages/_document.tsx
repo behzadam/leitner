6adb69583a68a584cd533dd8f4d9bfad50fefc55
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-25 15:22:37 +0430

    feat: add main document

---
+++ apps/ui/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MainDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MainDocument
