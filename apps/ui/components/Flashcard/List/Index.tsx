e675920b05f93cd8431beb1c1df587ef5496f4b9
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-30 23:00:47 +0430

    feat: update list with Paper

--- apps/ui/components/Flashcard/List/Index.tsx
+++ apps/ui/components/Flashcard/List/Index.tsx
@@ -1,30 +1,18 @@
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Box, Stack, Button, Container } from '@mui/material';
+import { Box, Paper, Stack, Button, Typography, Container } from '@mui/material';
 
 const columns: GridColDef[] = [
   {
     field: 'word',
-    headerName: 'Word',
+    headerName: 'Title',
     width: 70
-  },
-  {
-    field: 'translate',
-    headerName: 'Translate',
-    width: 300
-  },
-  {
-    field: 'description',
-    headerName: 'Description'
   }
 ];
 
-const rows = [
-  { id: 1, word: 'Word 1', translate: 'Word 1', description: 'Translate 1' },
-  { id: 2, word: 'Word 2', translate: 'Word 2', description: 'Translate 2' },
-  { id: 3, word: 'Word 3', translate: 'Word 3', description: 'Translate 3' },
-  { id: 4, word: 'Word 4', translate: 'Word 4', description: 'Translate 4' },
-  { id: 5, word: 'Word 5', translate: 'Word 5', description: 'Translate 5' },
-];
+const rows = [{
+  id: 1,
+  word: 'Word 1'
+}]
 
 const List = () => {
 
@@ -34,19 +22,24 @@ const List = () => {
 
   return (
     <Container maxWidth="md" sx={{ mt: 4 }}>
-      <Box sx={{ height: 315, width: '100%' }}>
-        <Stack direction="row" spacing={1} sx={{ mb: 1 }}>
+      <Box sx={{ height: 420, width: '100%' }}>
+        <Stack direction="row" justifyContent="space-between" spacing={1} sx={{ mb: 1, width: '100%' }}>
+          <Typography variant="h6" component="h6">
+            Flashcards
+          </Typography>
           <Button size="small" onClick={addRow} sx={{ ml: 'auto', mb: 1 }} color="primary" variant="contained">
             New Flashcard
           </Button>
         </Stack>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          checkboxSelection
-        />
+        <Paper elevation={1} sx={{ height: 420 }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={6}
+            rowsPerPageOptions={[6]}
+            checkboxSelection
+          />
+        </Paper>
       </Box>
     </Container >
   )
