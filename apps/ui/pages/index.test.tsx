28f46bb68575ede00ad2a7caa758893c7080c7d1
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-31 01:20:54 +0430

    test: move to near the index file

--- apps/ui/pages/index.test.tsx
+++ apps/ui/pages/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Index from '../pages/index';
+import Index from './index'
 import List from '@/components/Flashcard/List/Index';
 
 describe('Index', () => {
