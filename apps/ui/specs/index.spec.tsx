7f9393f54ca565b31f7e3e5db975ea6853f9ceb7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-30 15:54:01 +0430

    test: add list render test

--- apps/ui/specs/index.spec.tsx
+++ apps/ui/specs/index.spec.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Index from '../pages/index';
+import List from '@/components/Flashcard/List/Index';
 
 describe('Index', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Index />);
     expect(baseElement).toBeTruthy();
   });
+
+  it('should render List component', () => {
+    const { baseElement } = render(<List />);
+    expect(baseElement).toBeTruthy();
+  })
 });
