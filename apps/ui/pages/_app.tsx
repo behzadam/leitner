cb3f4ee90871932c6c9a273855442a0d7574a072
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-30 14:54:53 +0430

    refactor: remove styles

--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -1,6 +1,22 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,9 +24,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to ui!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <ThemeProvider theme={theme}>
+        <main>
+          <h2>Hello</h2>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </>
   );
 }
