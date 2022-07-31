506ca6eabd707769ee183b454b1598f88aa79a87
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-31 23:10:29 +0430

    test: add reduxt toolkit test util

---
+++ apps/ui/utils/test-utils.tsx
@@ -0,0 +1,34 @@
+// reference: https://redux.js.org/usage/writing-tests 
+import React, { PropsWithChildren } from 'react'
+import { render } from '@testing-library/react'
+import type { RenderOptions } from '@testing-library/react'
+import { configureStore } from '@reduxjs/toolkit'
+import type { PreloadedState } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
+import type { RootState, AppStore } from '@/store/index'
+
+import flashcardReducer from '@/store/flashcard-slice'
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>
+  store?: AppStore
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {
+      flashcard: undefined
+    },
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({ reducer: { flashcard: flashcardReducer }, preloadedState }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
