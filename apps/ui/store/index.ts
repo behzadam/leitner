dc80b04a1d7fbfb3e0ea4d14613267961f0e173f
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-07-31 23:11:16 +0430

    feat: add store ReturnType

--- apps/ui/store/index.ts
+++ apps/ui/store/index.ts
@@ -3,27 +3,41 @@ import {
   combineReducers,
   AnyAction,
   CombinedState,
+  ThunkAction,
 } from '@reduxjs/toolkit';
 import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 import { Reducer } from '@reduxjs/toolkit';
+import { Action } from 'redux';
+import flashcardReducer, { FlashcardState } from '@/store/flashcard-slice';
 
-export interface RootState {}
+export interface RootState {
+  flashcard: FlashcardState;
+}
 
-const RootReducer = (
+const combinedState = combineReducers({
+  flashcard: flashcardReducer,
+});
+
+const rootReducer = (
   state: RootState,
   action: AnyAction
 ): CombinedState<RootState> => {
   if (action.type === HYDRATE) return { ...state, ...action.payload };
-  const combinedReducer = combineReducers({
-    category: CurCategorySlice.reducer,
-  });
-  return combinedReducer(state, action);
+  return combinedState(state, action);
 };
 
-const makeStore = () =>
+export const makeStore = () =>
   configureStore({
-    reducer: RootReducer as Reducer<CombinedState<RootState>, AnyAction>,
+    reducer: rootReducer as Reducer<CombinedState<RootState>, AnyAction>,
     devTools: process.env.NODE_ENV === 'development',
   });
 
 export const wrapper = createWrapper(makeStore);
+export type AppStore = ReturnType<typeof makeStore>;
+export type AppState = ReturnType<AppStore['getState']>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action
+>;
