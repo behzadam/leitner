774642580528560f4ded9be8e0330e8ddb426ec5
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-05 02:18:31 +0430

    refactor: lint file

--- apps/ui/store/index.ts
+++ apps/ui/store/index.ts
@@ -1,13 +1,20 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import flashcardReducer from '@/features/flashcard/FlashcardSlice';
+import {
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
+import flashcardReducer from '@/features/flashcard/Flashcard.slice';
 import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
-  reducer: { flashcards: flashcardReducer },
+  reducer: {
+    flashcards: flashcardReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> =
+  useSelector;
