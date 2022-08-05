cd4a6d82cecccf6bcd8e7044ae1f5abdb23b5b7b
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-05 21:25:27 +0430

    feat: add delete api

--- apps/ui/features/flashcard/Flashcard.slice.ts
+++ apps/ui/features/flashcard/Flashcard.slice.ts
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import * as api from './Flashcard.api';
 
 export const fetchFlashcards = createAsyncThunk(
   'flashcard/fetchFlashcards',
@@ -9,11 +10,29 @@ export const fetchFlashcards = createAsyncThunk(
   }
 );
 
+export const fetchFlashcardById = createAsyncThunk(
+  'flashcard/fetchFlashcardById',
+  async (id: number) => {
+    const response = await api.fetchFlashcardById(id);
+    return response.data;
+  }
+);
+
 export const createFlashcard = createAsyncThunk(
   'flashcard/createFlashcard',
   async (params: any) => {
     const response = await axios.post('/flashcards', params);
     return response.data;
+  }
+);
+
+export const deleteFlashcard = createAsyncThunk(
+  'flashcard/deleteFlashcard',
+  async (id: number) => {
+    const item = await api.fetchFlashcardById(id);
+    if (!item.data) return;
+    await axios.delete(`/flashcards/${id}`);
+    return id;
   }
 );
 
@@ -35,6 +54,13 @@ const flashcardSlice = createSlice({
       })
       .addCase(createFlashcard.fulfilled, (state, action) => {
         state.items.push(action.payload);
+      })
+      .addCase(deleteFlashcard.fulfilled, (state, action) => {
+        const index = state.items.findIndex(
+          ({ id }) => id === action.payload
+        );
+        if (index < 0) return;
+        state.items.splice(index, 1);
       });
   },
   reducers: undefined,
