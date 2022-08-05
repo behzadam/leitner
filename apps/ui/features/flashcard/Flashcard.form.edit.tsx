f989982e49a37fe1f3c1844af28c81a016e363f3
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-05 21:24:12 +0430

    feat: add edit form

---
+++ apps/ui/features/flashcard/Flashcard.form.edit.tsx
@@ -0,0 +1,34 @@
+import { Box, TextField, Stack, Button } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { isEmpty } from 'utils/isEmpty';
+
+type FlashcardFormProps = {
+  // TODO: should replace any with DTO
+  onSubmit: (data: any) => void
+}
+
+const FlashcardForm = ({ onSubmit }: FlashcardFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Stack spacing={2} sx={{ pt: 1 }}>
+        <TextField error={!isEmpty(errors.word)} helperText="The title is required!" {...register('word', { required: true })} label="Word" variant="outlined" />
+        <TextField {...register('translate')} label="Translate" variant="outlined" />
+        <TextField {...register('description')} id="description" label="Description" variant="outlined" />
+        <Button type="submit" variant="contained" data-testid="submit" >Save</Button>
+      </Stack>
+    </Box>
+  );
+}
+
+export default FlashcardForm;
