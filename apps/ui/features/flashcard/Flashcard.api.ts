cf0ae2e2765ba7336627cb1678cc7a02995d6eab
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-07 18:32:03 +0430

    feat: add updateFlashcard

--- apps/ui/features/flashcard/Flashcard.api.ts
+++ apps/ui/features/flashcard/Flashcard.api.ts
@@ -2,3 +2,11 @@ import axios from 'axios';
 
 export const fetchFlashcardById = async (id: number) =>
   await axios.get(`/flashcards/${id}`);
+
+//TODO: should replace any type with dto
+export const updateFlashcard = async (params: any) =>
+  await axios.patch(`/flashcards/${params.id}`, {
+    word: params.word,
+    translate: params.translate,
+    description: params.description,
+  });
