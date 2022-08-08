0bf2ba419e19e2738d80f15ca013f55b6e59a3ee
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-08 16:46:53 +0430

    refactor: remove ApiProperty #3

--- libs/types/src/lib/create-flashcard.dto.ts
+++ libs/types/src/lib/create-flashcard.dto.ts
@@ -1,22 +1,8 @@
 import { IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateFlashcardDto {
-  @ApiProperty({
-    example: 'Home',
-    description: 'The word you want to create a flashcard from.',
-  })
   @IsNotEmpty()
   word: string;
-
-  @ApiProperty({
-    example: 'The house, apartment, or place where you live.',
-    description: 'The translate.',
-  })
   translate: string;
-
-  @ApiProperty({
-    description: 'Optional',
-  })
   description?: string;
 }
