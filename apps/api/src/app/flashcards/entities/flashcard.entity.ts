b2101459bcbab4a26322b45532a10ae395410eb3
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-10 23:23:31 +0430

    chore: add BaseEntity #9

--- apps/api/src/app/flashcards/entities/flashcard.entity.ts
+++ apps/api/src/app/flashcards/entities/flashcard.entity.ts
@@ -1,16 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+} from 'typeorm';
 
 @Entity()
-export class Flashcard {
+export class Flashcard extends BaseEntity {
+  @ApiProperty({
+    description: 'Primary key as Flashcard ID',
+    example: 1,
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    description: 'Title of flashcard',
+    example: 'Hello',
+  })
   @Column()
   word: string;
 
+  @ApiProperty({
+    description: 'Translate of flashcard',
+    example: 'Hallo',
+  })
   @Column()
   translate: string;
 
+  @ApiProperty({
+    description: 'Description',
+    example: 'Description',
+  })
   @Column()
   description: string;
 }
