81de5be810dbabd2c282669789751ddeead2bfcc
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-10 23:24:13 +0430

    feat: add api response decorators #9

---
+++ apps/api/src/common/api-response.decorator.ts
@@ -0,0 +1,76 @@
+import { PaginatedDto } from '@leitner/types';
+import { applyDecorators, Type } from '@nestjs/common';
+import {
+  ApiExtraModels,
+  ApiOkResponse,
+  getSchemaPath,
+} from '@nestjs/swagger';
+
+export const ApiResponse = <TModel extends Type<any>>(
+  model: TModel
+) => {
+  return applyDecorators(
+    ApiOkResponse({
+      schema: {
+        title: 'ApiResponse',
+        allOf: [
+          { $ref: getSchemaPath(Response) },
+          {
+            properties: {
+              code: { type: 'number', default: 200 },
+              message: { type: 'string', default: 'success' },
+              data: {
+                $ref: getSchemaPath(model),
+              },
+            },
+          },
+        ],
+      },
+    })
+  );
+};
+
+export const ApiPaginatedResponse = <TModel extends Type<any>>(
+  model: TModel
+) => {
+  return applyDecorators(
+    ApiExtraModels(PaginatedDto),
+    ApiOkResponse({
+      schema: {
+        description: 'Successfully received model list',
+        allOf: [
+          { $ref: getSchemaPath(PaginatedDto) },
+          {
+            properties: {
+              code: { type: 'number', default: 200 },
+              message: { type: 'string', default: 'success' },
+              items: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model) },
+              },
+              meta: {
+                type: 'any',
+                default: {
+                  totalItems: 0,
+                  totalPages: 1,
+                  itemCount: 0,
+                  itemsPerPage: 0,
+                  currentPage: 1,
+                },
+              },
+              links: {
+                type: 'any',
+                default: {
+                  first: '',
+                  previous: '',
+                  next: '',
+                  last: '',
+                },
+              },
+            },
+          },
+        ],
+      },
+    })
+  );
+};
