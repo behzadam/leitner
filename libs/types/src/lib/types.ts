005ac01910ce49406ac120152771a5ab474fc9b0
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-10 23:07:15 +0430

    refactor: move to dto folder

--- libs/types/src/lib/types.ts
+++ libs/types/src/lib/types.ts
@@ -1,5 +1,7 @@
-export * from './flashcard/create-flashcard.dto';
-export * from './flashcard/update-flashcard.dto';
+export * from './flashcard/dto/create-flashcard.dto';
+export * from './flashcard/dto/update-flashcard.dto';
 
 export * from './user/create-user.dto';
 export * from './user/update-user.dto';
+
+export * from './utils/api-response';
