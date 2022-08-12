4449fddafaaa128cde058706f83887c88560af45
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-12 22:36:51 +0430

    feat: re-export list-item-flashcard.dto

--- libs/types/src/lib/flashcard/index.ts
+++ libs/types/src/lib/flashcard/index.ts
@@ -1,3 +1,4 @@
 export * from './dto/create-flashcard.dto';
 export * from './dto/update-flashcard.dto';
+export * from './dto/list-item-flashcard.dto';
 export * from './entities/flashcard.entity';
