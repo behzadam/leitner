62f6927bbbdd9dd04770580033e76c34ff550659
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-14 01:15:31 +0430

    refactor: remove flashcard.entity re-export

--- libs/types/src/lib/flashcard/index.ts
+++ libs/types/src/lib/flashcard/index.ts
@@ -1,4 +1,3 @@
 export * from './dto/create-flashcard.dto';
 export * from './dto/update-flashcard.dto';
 export * from './dto/list-item-flashcard.dto';
-export * from './entities/flashcard.entity';
