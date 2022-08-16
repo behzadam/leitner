392c8a39f3260a57ca28430683a7aa742757d1a2
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-16 20:40:01 +0430

    refactor: remove items

--- apps/ui/pages/index.tsx
+++ apps/ui/pages/index.tsx
@@ -1,10 +1,9 @@
 import FlashcardList from '@ui/features/flashcard/Flashcard.list'
-import useFlashcardList from "@ui/features/flashcard/hooks/useFlashcardList";
 
 export function Index() {
-  const { items } = useFlashcardList();
+
   return (
-    <FlashcardList items={items} />
+    <FlashcardList />
   );
 }
 
