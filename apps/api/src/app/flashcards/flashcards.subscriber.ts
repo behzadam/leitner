bf6a5804ff349f05a0e987df2000b3da85b52239
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-21 19:54:22 +0430

    feat: add FlashcardSubscriber

---
+++ apps/api/src/app/flashcards/flashcards.subscriber.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Injectable, Logger } from '@nestjs/common';
+import { InsertEvent } from 'typeorm';
+import { EventSubscriber } from 'typeorm/decorator/listeners/EventSubscriber';
+import { EntitySubscriberInterface } from 'typeorm/subscriber/EntitySubscriberInterface';
+import { UpdateEvent } from 'typeorm/subscriber/event/UpdateEvent';
+import { Flashcard } from './entities/flashcard.entity';
+import { FlashcardHistory } from './entities/flashcard.history.entity';
+
+@Injectable()
+@EventSubscriber()
+export class FlashcardSubscriber
+  implements EntitySubscriberInterface<Flashcard>
+{
+  constructor() {}
+
+  listenTo() {
+    Logger.log('listenTo');
+    return Flashcard;
+  }
+
+  async onSave(
+    event: InsertEvent<Flashcard> | UpdateEvent<Flashcard>
+  ): Promise<void> {
+    try {
+      const flashcardHistory = new FlashcardHistory();
+      flashcardHistory.word = event.entity.word;
+      flashcardHistory.translate = event.entity.translate;
+      flashcardHistory.description = event.entity.description;
+      flashcardHistory.flashcard = event.entity as Flashcard;
+      flashcardHistory.flashcardId = event.entity.id;
+      await event.manager.save(flashcardHistory);
+    } catch (error) {
+      Logger.error(error);
+    }
+  }
+
+  async afterInsert(event: InsertEvent<Flashcard>) {
+    await this.onSave(event);
+  }
+
+  async beforeUpdate(event: UpdateEvent<Flashcard>) {
+    await this.onSave(event);
+  }
+}
