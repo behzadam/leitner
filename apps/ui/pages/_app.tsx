ef61b20085d46ca23d5cadfcfb4201fc93654474
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-30 13:40:59 +0430

    feat: change color palette

--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -7,6 +7,7 @@ import { store } from '../store/index';
 import Notification from '@ui/features/notification/Notification';
 
 import axios from "axios";
+import React from 'react';
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
 const theme = createTheme({
@@ -15,23 +16,41 @@ const theme = createTheme({
   },
   palette: {
     mode: 'dark',
-  },
+    primary: {
+      main: '#2c2c2c',
+    },
+    secondary: {
+      main: '#eab308',
+    },
+    divider: 'rgba(0,0,0,0.06)',
+    success: {
+      main: '#22c55e',
+    },
+    info: {
+      main: '#3b82f6',
+    },
+    warning: {
+      main: '#ca8a04',
+    },
+  }
 });
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <React.Fragment>
       <Head>
         <title>Leitner</title>
       </Head>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
+        <CssBaseline enableColorScheme />
         <Provider store={store}>
-          <Component {...pageProps} />
+          <main>
+            <Component {...pageProps} />
+          </main>
           <Notification />
         </Provider>
       </ThemeProvider>
-    </>
+    </React.Fragment>
   );
 }
 
