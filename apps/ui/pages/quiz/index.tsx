95e3f1082b6d79372a8062310006706c4367ae28
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-08-30 13:41:33 +0430

    refactor: remove main

--- apps/ui/pages/quiz/index.tsx
+++ apps/ui/pages/quiz/index.tsx
@@ -70,7 +70,6 @@ export default function ResponsiveDrawer(props: Props) {
         {drawer}
       </Drawer>
       <Box
-        component="main"
         sx={{ flexGrow: 1 }}
       >
         <Toolbar />
