83a9f825c433ba06aa8e5d8805e7a6937bb1f4df
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-09-10 22:43:19 +0430

    feat: separate boxes

--- apps/ui/features/leitner/Leitner.tsx
+++ apps/ui/features/leitner/Leitner.tsx
@@ -1,30 +1,37 @@
-import { Paper, Stack } from "@mui/material";
+import { Box, Card, CardContent, Stack, Typography } from "@mui/material";
 import { styled } from '@mui/material/styles';
+import StyleTwoToneIcon from '@mui/icons-material/StyleTwoTone';
 
-const Item = styled(Paper)(({ theme }) => ({
-  textAlign: 'center',
-  fontSize: '11px',
-  color: theme.palette.text.secondary,
-  borderRadius: '0px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  border: 'none'
+const Item = styled(CardContent)(({ theme }) => ({
+  marginBottom: '10px'
 }));
 
 const Leitner = (): JSX.Element => {
+  const leitnerBox = new Array(5).fill(0);
+  const leitnerBoxColors = ['#9ca3af', '#fde68a', '#fdba74', '#60a5fa', '#14b8a6'];
   return (
-    <Paper sx={{ maxWidth: '120px', mx: 'auto' }}>
-      <Stack
-        sx={{ margin: 'auto', borderRadius: '4px', overflow: 'hidden' }}
-      >
-        <Item sx={{ height: 90, backgroundColor: '#ecfccb' }}>5</Item>
-        <Item sx={{ height: 55, backgroundColor: '#cffafe' }}>4</Item>
-        <Item sx={{ height: 45, backgroundColor: '#fffbeb' }}>3</Item>
-        <Item sx={{ height: 35, backgroundColor: '#f5f5f5' }}>2</Item>
-        <Item sx={{ height: 30, backgroundColor: '#fafafa' }}>1</Item>
-      </Stack>
-    </Paper>
+    <Stack
+      sx={{ margin: 'auto', borderRadius: '0px', overflow: 'hidden' }}
+    >
+      {
+        leitnerBox.map((_, index) => {
+          return (
+            <Card variant="outlined" key={index} sx={{ marginBottom: 1 }}>
+              <CardContent>
+                <Stack
+                  direction="row"
+                  alignItems="center">
+                  <StyleTwoToneIcon sx={{ mr: 2, color: leitnerBoxColors[index] }} />
+                  <Typography variant="caption">Cards: {index + 1}</Typography>
+                  <Typography variant="caption" sx={{ ml: 2 }}>Ready: {index + 3}</Typography>
+                </Stack>
+              </CardContent>
+            </Card>
+          )
+        })
+      }
+      <Typography variant="caption">Items: 110</Typography>
+    </Stack >
   )
 }
 
