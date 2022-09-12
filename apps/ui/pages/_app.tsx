c170c122bbc68394b16dd6647e20c9cb3361ba9e
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-09-12 23:28:42 +0430

    feat: update theme / layput

--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -10,62 +9,26 @@ import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import axios from "axios";
 import React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AppPropsWithLayout } from '@ui/types';
+import { defaultTheme } from '../theme'
+
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const theme = createTheme({
-  typography: {
-    fontSize: 13,
-  },
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#2c2c2c',
-    },
-    secondary: {
-      main: '#eab308',
-    },
-    divider: 'rgba(0,0,0,0.07)',
-    success: {
-      main: '#22c55e',
-    },
-    info: {
-      main: '#3b82f6',
-    },
-    warning: {
-      main: '#ca8a04',
-    },
-  },
-  components: {
-    MuiToolbar: {
-      styleOverrides: {
-        dense: {
-          height: 48,
-          minHeight: 48
-        }
-      }
-    },
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          backgroundColor: "#fafafa"
-        }
-      }
-    }
-  }
-});
-
-function CustomApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <React.Fragment>
       <Head>
         <title>Leitner</title>
       </Head>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={defaultTheme}>
         <CssBaseline enableColorScheme />
         <Provider store={store}>
           <LocalizationProvider dateAdapter={AdapterMoment}>
             <main>
-              <Component {...pageProps} />
+              {
+                getLayout(<Component {...pageProps} />)
+              }
             </main>
           </LocalizationProvider>
           <Notification />
@@ -75,4 +38,4 @@ function CustomApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default CustomApp;
+export default App;
