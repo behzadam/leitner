98d2772324b559d60fc55c8fefbe27c8f99f4876
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-09-13 22:51:50 +0430

    feat: add DataGrid

--- apps/ui/features/flashcard/FlashcardListDemo.tsx
+++ apps/ui/features/flashcard/FlashcardListDemo.tsx
@@ -1,10 +1,86 @@
+import { Box, Container, IconButton, LinearProgress } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import InfoIcon from '@mui/icons-material/Info';
+import NoRows from '@ui/components/NoRows';
+import Link from 'next/link';
 
+const columns: GridColDef[] = [
+  {
+    field: 'id',
+    headerName: 'ID',
+    width: 90
+  },
+  {
+    field: 'front',
+    headerName: 'Front',
+    width: 150
+  },
+  {
+    field: 'back',
+    headerName: 'Back'
+  },
+  {
+    field: "actions",
+    headerName: "Actions",
+    minWidth: 120,
+    sortable: false,
+    disableColumnMenu: true,
+    renderCell: (params) => {
+      return (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Link
+            href={{
+              pathname: `/detail/[id]`,
+              query: { id: params.id }
+            }}
+          >
+            <a>
+              <IconButton>
+                <InfoIcon fontSize="small" />
+              </IconButton>
+            </a>
+          </Link>
+        </Box>
+      );
+    }
+  }
+]
 
+const rows = [
+  { id: 1, front: 'Front 1', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 2, front: 'Front 2', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 3, front: 'Front 3', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 4, front: 'Front 4', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 5, front: 'Front 5', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 6, front: 'Front 6', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 7, front: 'Front 7', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 8, front: 'Front 8', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+  { id: 9, front: 'Front 9', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
+];
 
 const FlashcardListDemo = (): JSX.Element => {
-
   return (
-    <p>FlashcardListDemo</p>
+    <Container maxWidth="md" sx={{ height: 422, mt: 4 }}>
+      <DataGrid
+        components={{
+          NoRowsOverlay: NoRows,
+          LoadingOverlay: LinearProgress,
+        }}
+        rows={rows}
+        columns={columns}
+        pageSize={6}
+        rowsPerPageOptions={[6]}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Container>
   )
 }
 
