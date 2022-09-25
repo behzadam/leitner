45a0f126e6d059b158e0e1529e0bfb2406ca5484
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-09-25 22:25:01 +0330

    feat: add link to items

--- apps/ui/features/category/CategoryList.tsx
+++ apps/ui/features/category/CategoryList.tsx
@@ -3,6 +3,8 @@ import { experimentalStyled as styled } from '@mui/material/styles';
 import { Paper, Container, Typography, Box } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -12,12 +14,13 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
   alignItems: 'center',
   justifyContent: 'center',
-  display: 'flex'
+  display: 'flex',
+  textDecoration: 'none'
 }));
 
 const Add = styled(Box)(({ theme }) => ({
   ...theme.typography.body2,
-  padding: theme.spacing(2),
+  padding: theme.spacing(1.7),
   textAlign: 'center',
   alignItems: 'center',
   justifyContent: 'center',
@@ -31,19 +34,36 @@ const Add = styled(Box)(({ theme }) => ({
 
 
 const CategoryList = (): JSX.Element => {
-  const categories = Array.from(Array(5))
+  const [categories, setCategories] = useState(Array.from([
+    {
+      name: '1100 words'
+    },
+    {
+      name: '504 words'
+    }
+  ]))
+
+  const handleAddCategory = (): void => {
+    setCategories(oldCategories => [...oldCategories, {
+      name: 'New Flashcards'
+    }])
+  }
   return (
     <Container maxWidth="sm">
       <Grid container sx={{ mt: 4 }} spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {categories.map((_, index) => (
+        {categories.map((cat, index) => (
           <Grid xs={2} sm={4} md={4} key={index}>
-            <Item>
-              <Typography sx={{ ml: 1 }}>Flashcards</Typography>
-            </Item>
+            <Link href="/flashcards">
+              <a>
+                <Item>
+                  <Typography variant="body2" sx={{ ml: 1 }}>{cat.name}</Typography>
+                </Item>
+              </a>
+            </Link>
           </Grid>
         ))}
         <Grid xs={2} sm={4} md={4}>
-          <Add>
+          <Add onClick={handleAddCategory}>
             <AddTwoToneIcon />
           </Add>
         </Grid>
