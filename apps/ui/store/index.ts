5f76676073d3e8f6a8c5942b8407c918561bce1a
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-09-29 03:41:55 +0330

    feat: add dialog slice

--- apps/ui/store/index.ts
+++ apps/ui/store/index.ts
@@ -3,16 +3,32 @@ import {
   useDispatch,
   useSelector,
 } from 'react-redux';
-import flashcardReducer from '@ui/features/flashcard/flashcardSlice';
+
 import { NotificationReducer } from '@ui/features/notification/notificationSlice';
 import { configureStore } from '@reduxjs/toolkit';
+import { ThunkExtraArguments } from '@ui/types';
+import dialogConfirmSlice from '@ui/components/dialog/dialogConfirmSlice';
+import flashcardReducer from '@ui/features/flashcard/flashcardSlice';
 
+const thunkArguments = {} as ThunkExtraArguments;
+const customizeMiddleware = () => {
+  return {
+    thunk: {
+      extraArgument: thunkArguments,
+    },
+  };
+};
 export const store = configureStore({
   reducer: {
     flashcards: flashcardReducer,
     notification: NotificationReducer,
+    dialogConfirm: dialogConfirmSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware(customizeMiddleware()),
 });
+
+thunkArguments.store = store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
