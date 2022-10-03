9acf106ef5685301f03a0ec3f9276ed2e03078f0
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-04 02:40:52 +0330

    refactor: with context api

--- apps/ui/features/flashcard/list/FlashcardListDelete.tsx
+++ apps/ui/features/flashcard/list/FlashcardListDelete.tsx
@@ -1,11 +1,21 @@
 import { IconButton } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import useFlashcardDelete from "../hooks/useFlashcardDelete";
+import { useFlashcardListContext } from "./FlashcardListDemo";
 
 const FlashcardListDelete = (): JSX.Element => {
+  const { currentRows } = useFlashcardListContext();
   const [disableButton, setDisableButton] = useState<boolean>(true);
   const { handleDeleteRows } = useFlashcardDelete();
+
+  useEffect(() => {
+    if (currentRows.length > 0) {
+      setDisableButton(false)
+    } else {
+      setDisableButton(true)
+    }
+  }, [currentRows])
 
   return (
     <IconButton aria-label="delete" sx={{ ml: 1 }} onClick={handleDeleteRows} size="small" color="error" disabled={disableButton}>
