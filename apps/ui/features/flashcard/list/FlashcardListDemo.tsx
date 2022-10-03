630e83eacd86c8162d09b8b7e18ee8032944dec3
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-04 02:09:47 +0330

    feat: add context / provider

--- apps/ui/features/flashcard/list/FlashcardListDemo.tsx
+++ apps/ui/features/flashcard/list/FlashcardListDemo.tsx
@@ -1,155 +1,82 @@
-import { Container, LinearProgress, Stack, Button, Typography, IconButton } from '@mui/material';
-import { DataGrid, GridColDef, GridSelectionModel } from '@mui/x-data-grid';
-import NoRows from '@ui/components/NoRows';
-import { useMemo, useState, useEffect } from 'react';
-import FlashcardListDemoActions from './FlashcardListDemoActions';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Link from 'next/link';
-import FlashcardCreateDialog from '../FlashcardCreateDialog';
-import FlashcardEditDialog from '../FlashcardEditDialog';
-import Show from '@ui/components/Show';
-import useFlashcardDelete from '../hooks/useFlashcardDelete';
+import { Container } from '@mui/material';
+import { useState, useEffect, createContext, Dispatch, useReducer, useContext } from 'react';
+import FlashcardListToolbar from './FlashcardListToolbar';
+import FlashcardListDataGrid from './FlashcardListDataGrid';
+import { useMemo } from 'react';
 
-const rows = [
-  { id: 1, front: 'Front 1', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 2, front: 'Front 2', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 3, front: 'Front 3', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 4, front: 'Front 4', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 5, front: 'Front 5', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 6, front: 'Front 6', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 7, front: 'Front 7', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 8, front: 'Front 8', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-  { id: 9, front: 'Front 9', back: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.' },
-];
-
-const CreateFlashcard = (): JSX.Element => {
-  const [open, setOpen] = useState<boolean>(false);
-  return (
-    <>
-      <Button size="small" onClick={() => setOpen(true)} color="primary" variant="contained" disableElevation>
-        New
-      </Button>
-      <FlashcardCreateDialog open={open} onClose={() => setOpen(false)} />
-    </>
-  )
+export type State = {
+  currentRow?: number;
+  currentRows: number[]
 }
 
-const EditFlashcard = ({
-  currentRowId,
-  setCurrentRowId
-}: {
-  currentRowId?: number,
-  setCurrentRowId: (id?: number) => void
-}): JSX.Element => {
-  const [open, setOpen] = useState<boolean>(false);
+export type Actions =
+  | { type: 'setCurrentRow', row: number | null }
+  | { type: 'setCurrentRows', rows: number[] };
 
-  useEffect(() => {
-    if (!currentRowId) return setOpen(false);
-    setOpen(true);
-  }, [currentRowId])
+export type DispatchContext = {
+  setCurrentRow: (row?: number) => void;
+  setCurrentRows: (rows: number[]) => void;
+}
 
-  const onClose = (): void => {
-    setCurrentRowId(null)
-    setOpen(false)
+export const initialState: State = {
+  currentRows: [],
+  currentRow: null
+}
+
+const reducer = (state: State, action: Actions): State => {
+  switch (action.type) {
+    case 'setCurrentRow':
+      return { ...state, currentRow: action.row }
+    case 'setCurrentRows':
+      return { ...state, currentRows: action.rows }
+    default:
+      return initialState;
   }
-
-  return (
-    <Show when={open}>
-      <FlashcardEditDialog id={currentRowId} open={open} onClose={onClose} />
-    </Show>
-  )
 }
 
-const DeleteFlashcards = (): JSX.Element => {
-  const [disableButton, setDisableButton] = useState<boolean>(true);
-  const { handleDeleteRows } = useFlashcardDelete();
+const FlashcardListContext = createContext<State>({} as State);
+const FlashcardListDispatchContext = createContext<DispatchContext>({} as DispatchContext);
+
+const FlashcardListProvider = ({ children }: { children?: React.ReactNode }): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const dispatchContext: DispatchContext = useMemo(() => {
+    const setCurrentRow = (row?: number): void => {
+      console.log('dispatchContext', row)
+      dispatch({ type: 'setCurrentRow', row })
+    }
+
+    const setCurrentRows = (rows: number[]): void => {
+      console.log('dispatchContext', rows)
+      dispatch({ type: 'setCurrentRows', rows })
+    }
+
+    return {
+      setCurrentRow,
+      setCurrentRows
+    }
+  }, [])
 
   return (
-    <IconButton aria-label="delete" sx={{ ml: 1 }} onClick={handleDeleteRows} size="small" color="error" disabled={disableButton}>
-      <DeleteIcon />
-    </IconButton>
-  )
-}
-
-const RedirectToQuiz = (): JSX.Element => {
-  return (
-    <Link href="/quiz">
-      <Button size="small" color="secondary" variant="contained" disableElevation>Quiz</Button>
-    </Link>
-  )
-}
-
-const FlashcardListDemoToolbar = (): JSX.Element => {
-  return (
-    <Stack direction="row" alignItems="center" sx={{ mb: 1 }}>
-      <Typography variant="h6" >
-        Flashcards
-      </Typography>
-      <DeleteFlashcards />
-      <Stack direction="row" alignItems="center" spacing={1} sx={{ ml: 'auto' }}>
-        <RedirectToQuiz />
-        <CreateFlashcard />
-      </Stack>
-    </Stack>
+    <FlashcardListContext.Provider value={state}>
+      <FlashcardListDispatchContext.Provider value={dispatchContext}>
+        {children}
+      </FlashcardListDispatchContext.Provider>
+    </FlashcardListContext.Provider>
   )
 }
 
 const FlashcardListDemo = (): JSX.Element => {
-  const [currentRowId, setCurrentRowId] = useState<number | null>(null);
-  const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
-  const columns: GridColDef[] = useMemo(() => [
-    {
-      field: 'id',
-      headerName: 'ID',
-      width: 90
-    },
-    {
-      field: 'front',
-      headerName: 'Front',
-      width: 150
-    },
-    {
-      field: 'back',
-      headerName: 'Back',
-      flex: 1
-    },
-    {
-      field: "actions",
-      headerName: "Actions",
-      minWidth: 120,
-      sortable: false,
-      disableColumnMenu: true,
-      renderCell: (params) => <FlashcardListDemoActions setCurrentRowId={setCurrentRowId} currentRowId={params.row.id} />
-    }
-  ], [])
-
   return (
-    <Container maxWidth="md" sx={{ height: 422, mt: 4 }}>
-      <FlashcardListDemoToolbar />
-      <DataGrid
-        components={{
-          NoRowsOverlay: NoRows,
-          LoadingOverlay: LinearProgress,
-        }}
-        rows={rows}
-        columns={columns}
-        pageSize={6}
-        rowsPerPageOptions={[6]}
-        checkboxSelection
-        disableSelectionOnClick
-        experimentalFeatures={{ newEditingApi: true }}
-        sx={{ backgroundColor: "white" }}
-        onSelectionModelChange={(newSelectionModel) => {
-          setSelectionModel(newSelectionModel);
-        }}
-        selectionModel={selectionModel}
-      />
-      <EditFlashcard
-        currentRowId={currentRowId}
-        setCurrentRowId={setCurrentRowId}
-      />
-    </Container>
+    <FlashcardListProvider>
+      <Container maxWidth="md" sx={{ height: 422, mt: 4 }}>
+        {/* <FlashcardListToolbar /> */}
+        <FlashcardListDataGrid />
+      </Container>
+    </FlashcardListProvider>
   )
 }
 
 export default FlashcardListDemo;
+export const useFlashcardListContext = (): State => useContext(FlashcardListContext)
+export const useFlashcardListDispatchContext = (): DispatchContext => useContext(FlashcardListDispatchContext)
