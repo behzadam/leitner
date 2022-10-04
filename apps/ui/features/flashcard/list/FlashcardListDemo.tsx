1881e6985224090b0b3064ca3ced88580b4dc38c
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-05 01:21:20 +0330

    refactor: with provider component

--- apps/ui/features/flashcard/list/FlashcardListDemo.tsx
+++ apps/ui/features/flashcard/list/FlashcardListDemo.tsx
@@ -1,70 +1,8 @@
 import { Container } from '@mui/material';
-import { createContext, useContext, useMemo, useReducer } from 'react';
 
+import FlashcardListProvider from '../provider/FlashcardListProvider';
 import FlashcardListDataGrid from './FlashcardListDataGrid';
 import FlashcardListToolbar from './FlashcardListToolbar';
-
-export type State = {
-  currentRow?: number;
-  currentRows: number[]
-}
-
-export type Actions =
-  | { type: 'setCurrentRow', row: number | null }
-  | { type: 'setCurrentRows', rows: number[] };
-
-export type DispatchContext = {
-  setCurrentRow: (row?: number) => void;
-  setCurrentRows: (rows: number[]) => void;
-}
-
-export const initialState: State = {
-  currentRows: [],
-  currentRow: null
-}
-
-const reducer = (state: State, action: Actions): State => {
-  switch (action.type) {
-    case 'setCurrentRow':
-      return { ...state, currentRow: action.row }
-    case 'setCurrentRows':
-      return { ...state, currentRows: action.rows }
-    default:
-      return initialState;
-  }
-}
-
-const FlashcardListContext = createContext<State>({} as State);
-const FlashcardListDispatchContext = createContext<DispatchContext>({} as DispatchContext);
-
-const FlashcardListProvider = ({ children }: { children?: React.ReactNode }): JSX.Element => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const dispatchContext: DispatchContext = useMemo(() => {
-    const setCurrentRow = (row?: number): void => {
-      console.log('dispatchContext', row)
-      dispatch({ type: 'setCurrentRow', row })
-    }
-
-    const setCurrentRows = (rows: number[]): void => {
-      console.log('dispatchContext', rows)
-      dispatch({ type: 'setCurrentRows', rows })
-    }
-
-    return {
-      setCurrentRow,
-      setCurrentRows
-    }
-  }, [])
-
-  return (
-    <FlashcardListContext.Provider value={state}>
-      <FlashcardListDispatchContext.Provider value={dispatchContext}>
-        {children}
-      </FlashcardListDispatchContext.Provider>
-    </FlashcardListContext.Provider>
-  )
-}
 
 const FlashcardListDemo = (): JSX.Element => {
   return (
@@ -78,5 +16,3 @@ const FlashcardListDemo = (): JSX.Element => {
 }
 
 export default FlashcardListDemo;
-export const useFlashcardListContext = (): State => useContext(FlashcardListContext)
-export const useFlashcardListDispatchContext = (): DispatchContext => useContext(FlashcardListDispatchContext)
---
+++ apps/ui/features/flashcard/provider/FlashcardListProvider.tsx
@@ -0,0 +1,65 @@
+import { createContext, useMemo, useReducer } from 'react';
+
+export type FlashcardListState = {
+  currentRow?: number;
+  currentRows: number[]
+}
+
+export type FlashcardListAction =
+  | { type: 'setCurrentRow', row: number | null }
+  | { type: 'setCurrentRows', rows: number[] };
+
+export type FlashcardListEvent = {
+  setCurrentRow: (row?: number) => void;
+  setCurrentRows: (rows: number[]) => void;
+}
+
+export const initialState: FlashcardListState = {
+  currentRows: [],
+  currentRow: null
+}
+
+const reducer = (state: FlashcardListState, action: FlashcardListAction): FlashcardListState => {
+  switch (action.type) {
+    case 'setCurrentRow':
+      return { ...state, currentRow: action.row }
+    case 'setCurrentRows':
+      return { ...state, currentRows: action.rows }
+    default:
+      return initialState;
+  }
+}
+
+export const FlashcardListContext = createContext<FlashcardListState>({} as FlashcardListState);
+export const FlashcardListEventContext = createContext<FlashcardListEvent>({} as FlashcardListEvent);
+
+const FlashcardListProvider = ({ children }: { children?: React.ReactNode }): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const dispatchContext: FlashcardListEvent = useMemo(() => {
+    const setCurrentRow = (row?: number): void => {
+      console.log('dispatchContext', row)
+      dispatch({ type: 'setCurrentRow', row })
+    }
+
+    const setCurrentRows = (rows: number[]): void => {
+      console.log('dispatchContext', rows)
+      dispatch({ type: 'setCurrentRows', rows })
+    }
+
+    return {
+      setCurrentRow,
+      setCurrentRows
+    }
+  }, [])
+
+  return (
+    <FlashcardListContext.Provider value={state}>
+      <FlashcardListEventContext.Provider value={dispatchContext}>
+        {children}
+      </FlashcardListEventContext.Provider>
+    </FlashcardListContext.Provider>
+  )
+}
+
+export default FlashcardListProvider;
