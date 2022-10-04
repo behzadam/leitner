45cceca5714d7611263b958fc550074429d16a50
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-05 01:13:11 +0330

    feat: import FlashcardCreateDialog

--- apps/ui/features/flashcard/list/FlashcardListCreate.tsx
+++ apps/ui/features/flashcard/list/FlashcardListCreate.tsx
@@ -1,15 +1,17 @@
-import { Button } from "@mui/material";
-import { useState } from "react";
+import { Button } from '@mui/material';
+import { Fragment, useState } from 'react';
+
+import FlashcardCreateDialog from '../FlashcardCreateDialog';
 
 const FlashcardListCreate = (): JSX.Element => {
   const [open, setOpen] = useState<boolean>(false);
   return (
-    <>
+    <Fragment>
       <Button size="small" onClick={() => setOpen(true)} color="primary" variant="contained" disableElevation>
         New
       </Button>
       <FlashcardCreateDialog open={open} onClose={() => setOpen(false)} />
-    </>
+    </Fragment>
   )
 }
 
