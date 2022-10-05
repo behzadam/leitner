687643ace5589f4778f703b446b5376b542ecfa9
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-05 17:46:38 +0330

    refactor: move create form to dialog

--- apps/ui/features/flashcard/form/FlashcardFormCreateDialog.tsx
+++ apps/ui/features/flashcard/form/FlashcardFormCreateDialog.tsx
@@ -1,6 +1,7 @@
-import { Dialog, DialogContent } from '@mui/material';
+import { Box, Button, Dialog, DialogContent, Divider, Stack } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import FlashcardTextField from './FlashcardTextField';
 
-import FlashcardFormCreate from './FlashcardFormCreate';
 
 const maxWidth = 'xs';
 
@@ -14,10 +15,38 @@ const FlashcardFormCreateDialog = ({
   onClose
 }: FlashcardCreateDialogProps): JSX.Element => {
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (): void => {
+    console.log('create form onSubmit')
+    onClose();
+  }
+
   return (
     <Dialog onClose={onClose} open={open} fullWidth={true} maxWidth={maxWidth}>
       <DialogContent sx={{ p: 0 }}>
-        <FlashcardFormCreate />
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Stack
+            direction="column"
+            divider={<Divider orientation="horizontal" flexItem />}
+          >
+            <FlashcardTextField variant="outlined" placeholder="Front" />
+            <FlashcardTextField variant="outlined" placeholder="Back" />
+            <Box sx={{ display: 'flex' }}>
+              <Button type="button" onClick={onClose} sx={{ borderRadius: 0, py: 2, flex: 1 }}>Cancel</Button>
+              <Button type="submit" variant="contained" sx={{ borderRadius: 0, py: 2, flex: 1 }}>Save</Button>
+            </Box>
+          </Stack>
+        </Box>
       </DialogContent>
     </Dialog>
   )
