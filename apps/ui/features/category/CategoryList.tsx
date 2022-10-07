3b2428fd5782fd528634a56de8bb48346b48fe32
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-07 04:35:08 +0330

    refactor: with new add component

--- apps/ui/features/category/CategoryList.tsx
+++ apps/ui/features/category/CategoryList.tsx
@@ -1,8 +1,7 @@
-import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
-import { Box, Container, Paper } from '@mui/material';
+import { Container, Paper } from '@mui/material';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
-import { useState } from 'react';
+import CategoryAddButton from './CategoryAddButton';
 import CategoryListItem from './CategoryListItem';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -17,48 +16,19 @@ const Item = styled(Paper)(({ theme }) => ({
   textDecoration: 'none'
 }));
 
-const Add = styled(Box)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1.7),
-  textAlign: 'center',
-  alignItems: 'center',
-  justifyContent: 'center',
-  display: 'flex',
-  borderRadius: '5px',
-  borderWidth: '1px',
-  borderStyle: 'dashed',
-  borderColor: theme.palette.mode === 'dark' ? '#1A2027' : '#ddd',
-  cursor: 'pointer'
-}));
-
-
 const CategoryList = (): JSX.Element => {
-  const [categories, setCategories] = useState(Array.from([
-    {
-      name: '1100 words'
-    },
-    {
-      name: '504 words'
-    }
-  ]))
+  const categories: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
 
-  const handleAddCategory = (): void => {
-    setCategories(oldCategories => [...oldCategories, {
-      name: 'New Flashcards'
-    }])
-  }
   return (
-    <Container maxWidth="sm">
-      <Grid container sx={{ mt: 4 }} spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 8 }}>
-        {categories.map((cat, index) => (
+    <Container maxWidth="md">
+      <Grid container sx={{ mt: 4 }} spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 8, lg: 12 }}>
+        {categories.map((_, index) => (
           <Grid xs={2} sm={4} md={4} key={index}>
             <CategoryListItem />
           </Grid>
         ))}
         <Grid xs={2} sm={4} md={4}>
-          <Add onClick={handleAddCategory}>
-            <AddTwoToneIcon />
-          </Add>
+          <CategoryAddButton />
         </Grid>
       </Grid>
     </Container>
