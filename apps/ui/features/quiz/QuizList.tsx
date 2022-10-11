684aeaffba1df147bec7eef70c84327678c557ec
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-11 18:26:08 +0330

    feat: add custom Chip

--- apps/ui/features/quiz/QuizList.tsx
+++ apps/ui/features/quiz/QuizList.tsx
@@ -1,7 +1,7 @@
 import AddIcon from '@mui/icons-material/Add';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import { Chip, Grid, IconButton, Paper } from '@mui/material';
+import { Chip, ChipProps, Grid, IconButton, Paper, styled } from '@mui/material';
 import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
 import { useTheme } from '@mui/material/styles';
@@ -18,6 +18,14 @@ type QuizListProps = {
   items?: any[]
   onCreate?: () => void
 }
+
+const ChipPlayButton = styled(Chip)<ChipProps>(({ theme }) => ({
+  boxShadow: 'inset 0px 0px 0px 2px',
+  backgroundColor: theme.palette.mode === 'dark' ? '#000' : '#fff',
+  paddingLeft: 4.2,
+  paddingRight: 4.2
+}))
+
 
 function QuizList({
   items = Array.of(1, 2, 3, 4, 5, 6, 7, 8)
@@ -101,7 +109,7 @@ function QuizList({
         </Grid>
         <Grid sx={{ py: 1, ml: 'auto' }}>
           <Show when={!autoplay}>
-            <Chip variant="filled" label="Start" onClick={handleAutoPlay} size="medium" sx={{ mr: 1 }} />
+            <ChipPlayButton variant="filled" label="Start" onClick={handleAutoPlay} size="medium" sx={{ mr: 1 }} />
           </Show>
           <Show when={autoplay}>
             <Chip variant="filled" label="Pause" onClick={handleAutoPlay} size="medium" sx={{ mr: 1 }} />
