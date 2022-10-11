b1128f1b1cbd127929b1ef892ebccdcfca6f37bf
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-11 21:21:45 +0330

    feat: add Stat page

---
+++ apps/ui/pages/stat/index.tsx
@@ -0,0 +1,13 @@
+import LayoutNested from "@ui/components/Layout/LayoutNested";
+
+
+const Index = (): JSX.Element => {
+
+  return (
+    <LayoutNested>
+      <p>Index</p>
+    </LayoutNested>
+  )
+};
+
+export default Index;
