b9b7ac963e7baa05cc3b5cbc1c02c8fd91cefed7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-12 18:19:52 +0330

    feat: add calendar / leitner box

--- apps/ui/pages/stat/index.tsx
+++ apps/ui/pages/stat/index.tsx
@@ -1,11 +1,22 @@
-import LayoutNested from "@ui/components/Layout/LayoutNested";
-
+import { Container, Grid } from '@mui/material';
+import LayoutNested from '@ui/components/Layout/LayoutNested';
+import Leitner from '@ui/features/leitner/Leitner';
+import ProgressCalendar from '@ui/features/progress/ProgressCalendar';
 
 const Index = (): JSX.Element => {
 
   return (
     <LayoutNested>
-      <p>Index</p>
+      <Container maxWidth="md">
+        <Grid container spacing={2} sx={{ mt: 4 }}>
+          <Grid item>
+            <ProgressCalendar />
+          </Grid>
+          <Grid item>
+            <Leitner />
+          </Grid>
+        </Grid>
+      </Container>
     </LayoutNested>
   )
 };
