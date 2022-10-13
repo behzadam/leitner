2c6d441d4a7ac772eadf98520d9e923ba7602e45
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-13 23:54:44 +0330

    feat: update with floating action

--- apps/ui/features/category/CategoryList.tsx
+++ apps/ui/features/category/CategoryList.tsx
@@ -5,19 +5,17 @@ import CategoryAddButton from './CategoryAddButton';
 import CategoryListItem from './CategoryListItem';
 
 const CategoryList = (): JSX.Element => {
-  const categories: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+  const categories: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="md" sx={{ paddingBottom: 11 }}>
       <Grid container sx={{ mt: 4 }} spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 8, lg: 12 }}>
         {categories.map((_, index) => (
           <Grid xs={2} sm={4} md={4} key={index}>
             <CategoryListItem />
           </Grid>
         ))}
-        <Grid xs={2} sm={4} md={4}>
-          <CategoryAddButton />
-        </Grid>
+        <CategoryAddButton />
       </Grid>
     </Container>
   )
