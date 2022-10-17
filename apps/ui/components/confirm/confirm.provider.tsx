f613078a797905c9c4839da46a07f08be34bdaf1
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-17 05:37:40 +0330

    feat: add confirm provider

---
+++ apps/ui/components/confirm/confirm.provider.tsx
@@ -0,0 +1,92 @@
+import { createContext, ReactNode, useContext, useMemo, useReducer } from 'react';
+import Confirm from "./confirm";
+
+export type ConfirmOptions = {
+  message?: string;
+  content?: ReactNode;
+  resolve?: (value: boolean) => void;
+}
+
+export type ConfirmState = {
+  isOpened: boolean;
+  isConfirmed: boolean;
+  isDeclined: boolean;
+  options?: ConfirmOptions;
+}
+
+export type ConfirmAction =
+  | { type: 'CONFIRM_OPENED', payload?: ConfirmOptions }
+  | { type: 'CONFIRM_CONFIRMED' }
+  | { type: 'CONFIRM_DECLINED' }
+
+export type ConfirmEvent = {
+  onOpen: (options?: ConfirmOptions) => void;
+  onDecline: () => void;
+  onConfirm: () => void;
+}
+
+const initialOptions: ConfirmOptions = {
+  message: null,
+  content: null,
+  resolve: null
+}
+
+const initialState: ConfirmState = {
+  isOpened: false,
+  isConfirmed: false,
+  isDeclined: false,
+  options: { ...initialOptions }
+}
+
+const reducer = (state: ConfirmState, action: ConfirmAction): ConfirmState => {
+  switch (action.type) {
+    case 'CONFIRM_OPENED':
+      return { ...state, isOpened: true, options: { ...action.payload } }
+    case 'CONFIRM_DECLINED':
+      return { ...initialState, isOpened: false, isDeclined: true }
+    case 'CONFIRM_CONFIRMED':
+      return { ...initialState, isOpened: false, isConfirmed: true }
+    default:
+      return state;
+  }
+}
+
+export const ConfirmContext = createContext<ConfirmState>({} as ConfirmState);
+export const ConfirmEvent = createContext<ConfirmEvent>({} as ConfirmEvent);
+
+const ConfirmProvider = ({ children }: { children?: React.ReactNode }): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const dispatchContext = useMemo(() => {
+    const onOpen = (options: Partial<ConfirmOptions>): void => {
+      dispatch({ type: 'CONFIRM_OPENED', payload: { ...options } })
+    }
+
+    const onDecline = (): void => {
+      dispatch({ type: 'CONFIRM_DECLINED' })
+    }
+
+    const onConfirm = (): void => {
+      dispatch({ type: 'CONFIRM_CONFIRMED' })
+    }
+
+    return {
+      onOpen,
+      onDecline,
+      onConfirm
+    }
+  }, [])
+
+  return (
+    <ConfirmContext.Provider value={state}>
+      <ConfirmEvent.Provider value={dispatchContext}>
+        {children}
+        <Confirm open={state.isOpened} />
+      </ConfirmEvent.Provider>
+    </ConfirmContext.Provider>
+  )
+}
+
+export const useConfirmContext = (): ConfirmState => useContext(ConfirmContext);
+export const useConfirmEvent = (): ConfirmEvent => useContext(ConfirmEvent);
+export default ConfirmProvider;
