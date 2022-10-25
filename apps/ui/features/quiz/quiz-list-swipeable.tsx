90f4156ccb9b650e75784dbe9a608301a9a280db
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-25 23:13:13 +0330

    refactor: remove Paper

--- apps/ui/features/quiz/quiz-list-swipeable.tsx
+++ apps/ui/features/quiz/quiz-list-swipeable.tsx
@@ -1,9 +1,8 @@
-import { Paper, useTheme } from '@mui/material';
+import { useTheme } from '@mui/material';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay, bindKeyboard } from 'react-swipeable-views-utils';
 import QuizListItem from './quiz-list-item';
 import { useQuizContext, useQuizEvent } from './quiz-provider';
-
 
 const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
 const AutoPlaySwipeableViews = autoPlay(BindKeyboardSwipeableViews);
@@ -17,32 +16,26 @@ const QuizListSwipeable = ({ items }: QuizListSwipeableProps): JSX.Element => {
   const { activeStep, autoplay } = useQuizContext();
   const theme = useTheme();
 
-  const onRemember = (id: number, rememberedIt: boolean): void => {
-    console.log('onRemember', id, rememberedIt)
-  }
-
   const handleStepChange = (step: number): void => {
     onSetActiveStep(step);
   };
 
   return (
-    <Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-        autoplay={autoplay}
-      >
-        {items.map((_, index) => (
-          <div key={index}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <QuizListItem onRemember={(id, value) => onRemember(id, value)} />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-    </Paper>
+    <AutoPlaySwipeableViews
+      axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+      index={activeStep}
+      onChangeIndex={handleStepChange}
+      enableMouseEvents
+      autoplay={autoplay}
+    >
+      {items.map((_, index) => (
+        <div key={index}>
+          {Math.abs(activeStep - index) <= 2 ? (
+            <QuizListItem />
+          ) : null}
+        </div>
+      ))}
+    </AutoPlaySwipeableViews>
   )
 }
 export default QuizListSwipeable;
