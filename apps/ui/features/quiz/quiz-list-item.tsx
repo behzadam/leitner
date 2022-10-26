cfcbc13c31a35b1928eec3d984be77e8d7c5d2c2
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-26 17:21:21 +0330

    feat: add cursor: grab

--- apps/ui/features/quiz/quiz-list-item.tsx
+++ apps/ui/features/quiz/quiz-list-item.tsx
@@ -6,7 +6,7 @@ const QuizListItem = (): JSX.Element => {
   const [showBack, setShowBack] = useState<boolean>(false);
 
   return (
-    <Box sx={{ minHeight: 270 }}>
+    <Box sx={{ minHeight: 270, cursor: 'grab' }}>
       <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', p: 3 }}>
         <Typography sx={{ fontSize: 16 }} variant="h6" component="div">
           In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
