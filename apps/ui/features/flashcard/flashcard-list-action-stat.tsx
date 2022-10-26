d7426b8e8c5f069a79bffd5bce91a7c36c5103e4
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-26 17:23:33 +0330

    fix: replace quiz with stat

--- apps/ui/features/flashcard/flashcard-list-action-stat.tsx
+++ apps/ui/features/flashcard/flashcard-list-action-stat.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const FlashcardListActionStat = (): JSX.Element => {
   return (
-    <Link href="/quiz">
+    <Link href="/stat">
       <IconButton size="medium">
         <PieChartOutlineOutlinedIcon />
       </IconButton>
