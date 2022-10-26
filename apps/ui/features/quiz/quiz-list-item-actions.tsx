79815c04f5e9339479ed80a310e8dc84d0455e91
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-26 15:46:33 +0330

    refactor: repace Box with Button

--- apps/ui/features/quiz/quiz-list-item-actions.tsx
+++ apps/ui/features/quiz/quiz-list-item-actions.tsx
@@ -1,11 +1,13 @@
-import { Box, BoxProps, Divider, Stack, styled } from "@mui/material";
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import { Button, ButtonProps, Divider, Stack, styled } from '@mui/material';
 
-const Item = styled(Box)<BoxProps>(() => ({
-  height: 35,
+const Item = styled(Button)<ButtonProps>(() => ({
+  height: 45,
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'center',
-  cursor: 'pointer',
+  justifyContent: 'center'
 }))
 
 const QuizListItemActions = (): JSX.Element => {
@@ -14,13 +16,19 @@ const QuizListItemActions = (): JSX.Element => {
       direction="row"
       alignItems="center"
       sx={{
-        py: 1,
+        p: 1,
       }}
       divider={<Divider orientation="vertical" flexItem />}
     >
-      <Item sx={{ flex: 0.5 }}>Cancel</Item>
-      <Item sx={{ flex: 0.5 }}>Show</Item>
-      <Item sx={{ flex: 1 }}>Ok</Item>
+      <Item sx={{ flex: 0.5 }}>
+        <CloseOutlinedIcon />
+      </Item>
+      <Item sx={{ flex: 0.5 }}>
+        <VisibilityOffOutlinedIcon />
+      </Item>
+      <Item sx={{ flex: 1 }} color="success">
+        <CheckOutlinedIcon color="success" />
+      </Item>
     </Stack>
   )
 }
