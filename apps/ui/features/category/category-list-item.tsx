ce19b91653fc2b5e7db00a17325b3f9b314c7618
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-29 23:56:04 +0330

    feat: add link to title

--- apps/ui/features/category/category-list-item.tsx
+++ apps/ui/features/category/category-list-item.tsx
@@ -1,8 +1,10 @@
+import OpenInNewTwoToneIcon from '@mui/icons-material/OpenInNewTwoTone';
 import {
   Card,
   CardActions,
   CardContent, Stack, Typography
 } from '@mui/material';
+import Link from '@ui/components/link/Link';
 import CategoryListActions from './category-list-actions';
 
 type CategoryListItemProps = {
@@ -16,7 +18,10 @@ const CategoryListItem = ({ id }: CategoryListItemProps): JSX.Element => {
     <Card>
       <CardContent>
         <Stack direction="row" alignItems="center">
-          <Typography variant="overline">Flashcard Name</Typography>
+          <Link href="/flashcards">
+            <Typography variant="overline">Flashcard Name</Typography>
+            <OpenInNewTwoToneIcon sx={{ fontSize: 10, ml: 1 }} />
+          </Link>
         </Stack>
       </CardContent>
       <CardActions>
