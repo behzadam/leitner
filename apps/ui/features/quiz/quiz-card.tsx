381174a80ed5b7505eb281a9ca08ed5caeb0e7ce
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-31 02:39:45 +0330

    feat: update card front

--- apps/ui/features/quiz/quiz-card.tsx
+++ apps/ui/features/quiz/quiz-card.tsx
@@ -3,21 +3,19 @@ import { animated } from '@react-spring/web';
 
 const Deck = animated(styled(Box)<BoxProps>(() => ({
   position: 'absolute',
-  maxWidth: '280px',
-  aspectRatio: '3 / 4',
-  willChange: 'transform',
+  width: '280px',
+  height: '373px',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  padding: '2rem',
+  padding: '1.5rem',
   backgroundColor: 'white',
   borderRadius: '0.3rem'
 })));
 
 const DeckContent = styled(Box)<BoxProps>(() => ({
   willChange: 'transform',
-  touchAction: 'none',
-  border: '2px solid #000'
+  touchAction: 'none'
 }));
 
 
@@ -25,7 +23,7 @@ const QuizCard = ({ ...props }): JSX.Element => {
   return (
     <Deck {...props}>
       <DeckContent>
-        <Typography sx={{ fontSize: 16 }} variant="h6" component="div">
+        <Typography variant="body2">
           In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
         </Typography>
       </DeckContent>
