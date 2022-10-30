5a30bfe7781c5876ab4559887ce835bc4765f4de
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-10-30 20:49:15 +0330

    feat: add QuizDeck component

--- apps/ui/features/quiz/quiz-list.tsx
+++ apps/ui/features/quiz/quiz-list.tsx
@@ -1,43 +1,20 @@
-import { Grid, Paper } from '@mui/material';
-import Box from '@mui/material/Box';
-import QuizListActions from './quiz-list-actions';
-import QuizListItemActions from './quiz-list-item-actions';
-import QuizListStepper from './quiz-list-stepper';
+import QuizDeck from './quiz-deck';
 
-import QuizListSwipeable from './quiz-list-swipeable';
-import QuizListToolbar from './quiz-list-toolbar';
 import QuizProvider from './quiz-provider';
-
-
 
 type QuizListProps = {
   items?: any[]
   onCreate?: () => void
 }
 
-
-function QuizList({
+const QuizList = ({
   items = Array.of(1, 2, 3, 4, 5, 6, 7, 8)
-}: QuizListProps) {
+}: QuizListProps): JSX.Element => {
   console.log({ items });
 
   return (
     <QuizProvider>
-      <Box sx={{ flexGrow: 1 }}>
-        <QuizListToolbar />
-        <Paper>
-          <QuizListSwipeable items={items} />
-          <QuizListItemActions />
-        </Paper>
-        <Grid container sx={{ mt: 1 }}>
-          <Grid sx={{ flex: 1 }}>
-            <QuizListStepper items={items} />
-          </Grid>
-          <Grid sx={{ py: 1, ml: 'auto' }}>
-            <QuizListActions />
-          </Grid>
-        </Grid>
-      </Box>
+      <QuizDeck />
     </QuizProvider>
   );
 }
