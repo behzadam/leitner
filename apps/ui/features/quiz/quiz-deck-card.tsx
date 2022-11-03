c8bafa06a81434bd904088ca1386f096e0e33cee
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-03 07:18:09 +0330

    feat: add flip card

--- apps/ui/features/quiz/quiz-deck-card.tsx
+++ apps/ui/features/quiz/quiz-deck-card.tsx
@@ -1,35 +1,76 @@
-import { Box, BoxProps, styled, Typography } from '@mui/material';
-import { animated } from '@react-spring/web';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import { Box, BoxProps, IconButton, IconButtonProps, styled } from '@mui/material';
+import { animated, useSpring } from '@react-spring/web';
+import Show from '@ui/components/Show';
+import { useState } from 'react';
 
-const Deck = animated(styled(Box)<BoxProps>(() => ({
-  position: 'absolute',
-  aspectRatio: '0.8',
+const FlipCard = animated(styled(Box)<BoxProps>(() => ({
+  width: '100%',
+  height: '100%',
   display: 'flex',
-  alignItems: 'center',
   justifyContent: 'center',
-  cursor: 'grab',
-  padding: '1.5rem',
-  backgroundColor: 'white',
+  alignItems: 'center',
+  position: 'absolute',
   borderRadius: '1.5rem',
-  userSelect: 'none'
+  userSelect: 'none',
+  cursor: 'grab',
+  backgroundColor: 'white',
 })));
 
-const DeckContent = styled(Box)<BoxProps>(() => ({
-  willChange: 'transform',
+const FlipCardContent = animated(styled(Box)<BoxProps>(() => ({
+  width: '100%',
+  height: '100%',
   touchAction: 'none',
-  userSelect: 'auto'
-}));
+  position: 'absolute',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  willChange: 'transform, opacity'
+})));
 
+const FlipCardAction = styled(IconButton)<IconButtonProps>(() => ({
+  marginTop: 'auto'
+}))
 
 const QuizDeckCard = ({ ...props }): JSX.Element => {
+  const [flipped, setFlipped] = useState<boolean>(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  const handleToggleCard = (): void => {
+    setFlipped(flipped => !flipped);
+  }
+
   return (
-    <Deck {...props}>
-      <DeckContent>
-        <Typography variant="body1">
-          In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
-        </Typography>
-      </DeckContent>
-    </Deck>
+    <FlipCard {...props}>
+      <FlipCardContent
+        style={{
+          opacity: opacity.to(op => 1 - op),
+          transform
+        }}>
+        <p>Front</p>
+      </FlipCardContent>
+      <FlipCardContent
+        style={{
+          opacity,
+          transform,
+          rotateY: '180deg'
+        }}>
+        <p>Back</p>
+      </FlipCardContent>
+      <FlipCardAction onClick={handleToggleCard}>
+        <Show when={!flipped}>
+          <RemoveRedEyeOutlinedIcon />
+        </Show>
+        <Show when={flipped}>
+          <VisibilityOffOutlinedIcon />
+        </Show>
+      </FlipCardAction>
+    </FlipCard>
   )
 }
 
