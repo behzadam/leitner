9ebdd939f044a2f247d66bda239cba843e65e89d
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-04 23:07:28 +0330

    refactor: rename component

--- apps/ui/features/quiz/quiz-toolbox.tsx
+++ apps/ui/features/quiz/quiz-toolbox.tsx
@@ -1,18 +1,13 @@
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
-import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 import { IconButton, Stack } from "@mui/material";
 
-const QuizActions = (): JSX.Element => {
-
+const QuizToolbox = (): JSX.Element => {
   return (
     <Stack direction="row" spacing={2}>
       <IconButton color="primary">
         <SettingsOutlinedIcon />
-      </IconButton>
-      <IconButton color="primary">
-        <PlayCircleFilledWhiteOutlinedIcon />
       </IconButton>
       <IconButton color="primary">
         <AddCircleOutlineOutlinedIcon />
@@ -21,4 +16,4 @@ const QuizActions = (): JSX.Element => {
   )
 }
 
-export default QuizActions;
+export default QuizToolbox;
