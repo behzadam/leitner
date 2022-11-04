6c329da3d1d39c0147485ea087e7160c20ff46c9
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-04 23:09:36 +0330

    refactor: add dispatch / zIndex

--- apps/ui/features/quiz/quiz-deck.tsx
+++ apps/ui/features/quiz/quiz-deck.tsx
@@ -1,22 +1,26 @@
 import { useSprings } from '@react-spring/web';
+import { useAppDispatch } from '@ui/store/index';
 import { Flashcard } from '@ui/types';
 import { useDrag } from '@use-gesture/react';
 import { useMemo, useState } from 'react';
 import QuizDeckCard from './quiz-deck-card';
+import { onSwiped } from './quiz-slice';
 
 type QuizDeckType = {
   cards: Flashcard[]
 }
 
 const QuizDeck = ({ cards }: QuizDeckType): JSX.Element => {
+  const dispatch = useAppDispatch();
   const memoizedCards = useMemo(() => {
     return {
-      cards
+      cards,
+      cardsTotal: cards.length
     }
   }, [cards])
 
   const [gone] = useState(() => new Set())
-  const [props, api] = useSprings(memoizedCards.cards.length, i => ({
+  const [springs, api] = useSprings(memoizedCards.cards.length, i => ({
     from: {
       x: 0,
       y: -100,
@@ -28,22 +32,24 @@ const QuizDeck = ({ cards }: QuizDeckType): JSX.Element => {
       y: i * -2,
       delay: i * 10,
       scale: 1,
-      shadow: i < 3 ? 5 : 0
+      shadow: i < 3 ? 5 : 0,
+      zIndex: memoizedCards.cardsTotal - i
     }
   }))
 
   const bind = useDrag(({ args: [index], active, movement: [mx], direction: [xDir], velocity: [vx] }) => {
     const trigger = vx > 0.2
-    if (!active && trigger) gone.add(index)
+    if (!active && trigger) {
+      gone.add(index);
+      dispatch(onSwiped());
+    }
     api.start(i => {
       if (index !== i) return;
+
       const isGone = gone.has(index);
       const x = isGone ? (200 + window.innerWidth) * xDir : active ? mx : 0;
-
-      //Todo: should replace with store card item
-      cards[i].isReady = isGone;
-
       const scale = active ? 1.05 : 1;
+
       return {
         x,
         scale,
@@ -56,16 +62,12 @@ const QuizDeck = ({ cards }: QuizDeckType): JSX.Element => {
   return (
     <>
       {
-        props.map(({ x, y, scale, shadow }, i) => (
+        springs.map((props, i) => (
           <QuizDeckCard
+            card={memoizedCards.cards[i]}
             key={i}
             {...bind(i)}
-            style={{
-              x,
-              y,
-              scale,
-              boxShadow: shadow.to(s => `rgba(0, 0, 0, 0.05) 0px ${s}px ${2 * s}px 0px`),
-            }} />
+            style={props} />
         ))
       }
     </>
