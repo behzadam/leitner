3abbe4e8138840d89770be5148702df4564e38b6
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-04 23:07:55 +0330

    refactor: repace const array with store

--- apps/ui/features/quiz/quiz-container.tsx
+++ apps/ui/features/quiz/quiz-container.tsx
@@ -1,9 +1,9 @@
 import { Box, BoxProps, styled } from '@mui/material';
 import PlaceholderNoRow from '@ui/components/placeholder/placeholder-no-row';
-import { Flashcard } from '@ui/types';
-import QuizActions from './quiz-actions';
+import { useAppSelector } from '@ui/store/index';
 import QuizDeck from './quiz-deck';
 import QuizProvider from './quiz-provider';
+import QuizToolbox from './quiz-toolbox';
 
 const QuizWrapper = styled(Box)<BoxProps>(() => ({
   width: '100vw',
@@ -32,26 +32,16 @@ const QuizInner = styled(Box)<BoxProps>(() => ({
 }));
 
 const QuizContainer = (): JSX.Element => {
-  const cards: Flashcard[] = [
-    { id: 1, front: 'Front card 1', back: 'Back card 1', isReady: true },
-    { id: 2, front: 'Front card 2', back: 'Back card 2', isReady: true },
-    { id: 3, front: 'Front card 3', back: 'Back card 3', isReady: true },
-    { id: 4, front: 'Front card 4', back: 'Back card 4', isReady: true },
-    { id: 5, front: 'Front card 5', back: 'Back card 5', isReady: false },
-    { id: 6, front: 'Front card 6', back: 'Back card 6', isReady: false },
-    { id: 7, front: 'Front card 7', back: 'Back card 7', isReady: false },
-    { id: 8, front: 'Front card 8', back: 'Back card 8', isReady: false },
-    { id: 9, front: 'Front card 9', back: 'Back card 9', isReady: false },
-  ]
+  const quizState = useAppSelector(state => state.quiz)
   return (
     <QuizProvider>
       <QuizWrapper>
         <QuizInner>
           <PlaceholderNoRow />
-          <QuizDeck cards={cards} />
+          <QuizDeck cards={quizState.cards} />
         </QuizInner>
         <Box sx={{ mt: 2 }}>
-          <QuizActions />
+          <QuizToolbox />
         </Box>
       </QuizWrapper>
     </QuizProvider>
