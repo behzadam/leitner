00a891f3f4a2c5176e8ef924bfc7a470f88b6883
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-06 23:51:52 +0330

    refactor: remove unused props

--- apps/ui/features/quiz/quiz-deck.tsx
+++ apps/ui/features/quiz/quiz-deck.tsx
@@ -2,7 +2,7 @@ import { useSprings } from '@react-spring/web';
 import { Flashcard } from '@ui/types';
 import { isNegative } from '@ui/utils/is-negative';
 import { useGesture } from '@use-gesture/react';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import QuizDeckCard from './quiz-deck-card';
 import { SwipeDirection, SwipeProps } from './quiz-types';
 
@@ -12,35 +12,21 @@ type QuizDeckProps = {
 }
 
 const QuizDeck = ({ cards, onSwiped }: QuizDeckProps): JSX.Element => {
-  console.log(cards);
-  const memoizedCards = useMemo(() => {
-    return {
-      cards,
-      cardsTotal: cards.length
-    }
-  }, [cards])
-
   const [gone] = useState(() => new Set())
-  const [springs, api] = useSprings(memoizedCards.cards.length, i => ({
+  const [springs, api] = useSprings(cards.length, i => ({
     from: {
       x: 0,
       y: -100,
-      scale: 0,
-      shadow: 0,
-      zIndex: 0
     },
     to: {
       x: 0,
-      y: i * -2,
-      delay: i * 10,
-      scale: 1,
-      shadow: i < 3 ? 5 : 0,
-      zIndex: i
+      y: 0,
+      zIndex: cards.length - i
     }
   }))
 
   console.log('springs', springs.length)
-  console.log('memoizedCards', memoizedCards.cards.length);
+  console.log('memoizedCards', cards.length);
 
   const bind = useGesture(
     {
@@ -53,18 +39,16 @@ const QuizDeck = ({ cards, onSwiped }: QuizDeckProps): JSX.Element => {
 
           const isGone = gone.has(index);
           const x = isGone ? (200 + window.innerWidth) * xDir : active ? mx : 0;
-          const scale = active ? 1.05 : 1;
 
           if (isGone) {
-            const id: number = memoizedCards.cards[index].id;
+            const id: number = cards[index].id;
             const direction: SwipeDirection = isNegative(x) ? 'left' : 'right';
-            onSwiped({ id, direction });
+            if (direction === 'left') {
+              onSwiped({ id, direction });
+            }
           }
-
           return {
             x,
-            scale,
-            delay: undefined,
             config: { friction: 50, tension: active ? 800 : isGone ? 200 : 500 },
           }
         })
@@ -76,8 +60,8 @@ const QuizDeck = ({ cards, onSwiped }: QuizDeckProps): JSX.Element => {
       {
         springs.map((props, i) => (
           < QuizDeckCard
-            card={memoizedCards.cards[i]}
-            key={`deck-${i}`}
+            card={cards[i]}
+            key={i}
             {...bind(i)}
             style={props}
           />
