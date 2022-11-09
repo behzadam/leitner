9e72e9f63167eaf717d19c4491823c48b9a56450
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-10 00:23:51 +0330

    refactor: split up to new action components

--- apps/ui/features/quiz/quiz-deck-actions.tsx
+++ apps/ui/features/quiz/quiz-deck-actions.tsx
@@ -1,43 +1,15 @@
-import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
-import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
-import { IconButton, Stack } from "@mui/material";
-import Show from '@ui/components/Show';
-import { useAppDispatch } from '@ui/store/index';
-import { useState } from 'react';
-import { cardFlipped } from './quiz-slice';
+import { Stack } from "@mui/material";
+import QuizDeckActionsFlip from './quiz-deck-actions-flip';
+import QuizDeckActionsNoRemember from "./quiz-deck-actions-no-remember";
+import QuizDeckActionsRemember from './quiz-deck-actions-remember';
 
 
 const QuizDeckActions = (): JSX.Element => {
-  const dispatch = useAppDispatch()
-  const [flipped, setFlipped] = useState<boolean>(false);
-
-  const handleFlipped = (): void => {
-    setFlipped(flipped => !flipped);
-    dispatch(cardFlipped());
-  }
-
-  const handleRemember = (): void => {
-    console.log('handleRemember')
-  }
-
   return (
     <Stack spacing={2} direction="row" sx={{ mt: 'auto', mb: 2 }}>
-      <IconButton color="error">
-        <CloseOutlinedIcon />
-      </IconButton>
-      <IconButton color="primary" onClick={handleFlipped}>
-        <Show when={!flipped}>
-          <RemoveRedEyeOutlinedIcon />
-        </Show>
-        <Show when={flipped}>
-          <VisibilityOffOutlinedIcon />
-        </Show>
-      </IconButton>
-      <IconButton color="success" onClick={handleRemember}>
-        <CheckOutlinedIcon />
-      </IconButton>
+      <QuizDeckActionsNoRemember />
+      <QuizDeckActionsFlip />
+      <QuizDeckActionsRemember />
     </Stack>
   )
 }
