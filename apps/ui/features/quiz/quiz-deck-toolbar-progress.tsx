05aeb6b4444e455b31652d862e3356a7e10958f7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-10 23:25:44 +0330

    feat: add progress component

---
+++ apps/ui/features/quiz/quiz-deck-toolbar-progress.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material";
+import Show from "@ui/components/Show";
+import { useAppSelector } from "@ui/store/index";
+import { selectCurrentIndex, selectFlashcardsCount } from "./quiz-slice";
+
+
+const QuizDeckToolbarProgress = (): JSX.Element => {
+  const currentIndex = useAppSelector(selectCurrentIndex);
+  const cardsCount = useAppSelector(selectFlashcardsCount);
+
+  return (
+    <Show when={currentIndex < cardsCount}>
+      <Typography variant="overline" sx={{ fontSize: '11px' }}>
+        {currentIndex} / {cardsCount}
+      </Typography>
+    </Show>
+  )
+}
+
+export default QuizDeckToolbarProgress;
