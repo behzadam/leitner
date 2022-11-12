304331635888a306acb1d2f25414a697745a0a37
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-12 15:51:00 +0330

    feat: remove action buttons

--- apps/ui/features/quiz/quiz-deck-toolbar.tsx
+++ apps/ui/features/quiz/quiz-deck-toolbar.tsx
@@ -1,21 +1,18 @@
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
-import { IconButton, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import QuizDeckToolbarProgress from './quiz-deck-toolbar-progress';
-import QuizDeckToolbarSettings from './quiz-deck-toolbar-settings';
 
 
 const QuizDeckToolbar = (): JSX.Element => {
   return (
-    <Stack direction="row" alignItems="center" sx={{ maxWidth: '320px', width: '100%', mb: 1 }}>
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ mb: 1 }}
+    >
       <Typography variant="overline" sx={{ fontWeight: 600, pr: 2 }}>Flashcards</Typography>
       <QuizDeckToolbarProgress />
-      <Stack direction="row" spacing={1} sx={{ ml: 'auto' }}>
-        <QuizDeckToolbarSettings />
-        <IconButton color="primary" size="medium">
-          <AddCircleOutlineOutlinedIcon sx={{ fontSize: '1.2rem' }} />
-        </IconButton>
-      </Stack>
     </Stack >
   )
 }
