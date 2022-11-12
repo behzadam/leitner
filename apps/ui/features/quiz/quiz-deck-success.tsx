68e35e55240a0d1c184a5f1a7e9c7480d7f39537
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-12 21:31:05 +0330

    feat: add remembered stat

--- apps/ui/features/quiz/quiz-deck-success.tsx
+++ apps/ui/features/quiz/quiz-deck-success.tsx
@@ -1,19 +1,24 @@
 import EmojiEventsTwoToneIcon from '@mui/icons-material/EmojiEventsTwoTone';
 import { Stack, Typography } from '@mui/material';
+import { useAppSelector } from '@ui/store/index';
+import { selectFlashcardsCount, selectRememeberedCount } from './quiz-slice';
 
 const QuizDeckSuccess = (): JSX.Element => {
+  const rememeberedCount = useAppSelector(selectRememeberedCount);
+  const cardsCount = useAppSelector(selectFlashcardsCount);
+
   return (
     <Stack spacing={2} direction="column" alignItems="center" sx={{ m: 'auto' }}>
       <EmojiEventsTwoToneIcon sx={{ fontSize: '4rem' }} color='success' />
       <Stack spacing={0.5} alignItems="center">
         <Typography fontWeight={700}>
-          Remembered: 20
+          Remembered: {rememeberedCount.yesCount}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Not remembered: 20
+          Not remembered: {rememeberedCount.noCount}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Items: 40
+          Items: {cardsCount}
         </Typography>
       </Stack>
     </Stack>
