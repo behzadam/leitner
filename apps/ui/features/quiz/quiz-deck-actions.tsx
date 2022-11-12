25486d07f852796ef70b9a6af90c9c028e9b3cf7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-12 23:44:30 +0330

    feat: add reload button

--- apps/ui/features/quiz/quiz-deck-actions.tsx
+++ apps/ui/features/quiz/quiz-deck-actions.tsx
@@ -4,24 +4,28 @@ import { useAppSelector } from '@ui/store/index';
 
 import QuizDeckActionsFlip from './quiz-deck-actions-flip';
 import QuizDeckActionsNoRemember from './quiz-deck-actions-no-remember';
+import QuizDeckActionsReload from './quiz-deck-actions-reload';
 import QuizDeckActionsRemember from './quiz-deck-actions-remember';
-import { selectCurrentIndex, selectFlashcardsCount } from './quiz-slice';
+import { selectIsDeckFinished } from './quiz-slice';
 
 type QuizDeckActionsProps = {
   onRemember: (index: number, isRemembered: boolean) => void;
 }
 
 const QuizDeckActions = ({ onRemember }: QuizDeckActionsProps): JSX.Element => {
-  const cardsCount = useAppSelector(selectFlashcardsCount);
-  const currentIndex = useAppSelector(selectCurrentIndex);
+  const isDeckFinished = useAppSelector(selectIsDeckFinished);
+
   return (
-    <Show when={currentIndex < cardsCount}>
-      <Stack spacing={2} direction="row" sx={{ mt: 2 }}>
+    <Stack spacing={2} direction="row" sx={{ mt: 2 }}>
+      <Show when={!isDeckFinished}>
         <QuizDeckActionsNoRemember onRemember={(index) => onRemember(index, false)} />
         <QuizDeckActionsRemember onRemember={(index) => onRemember(index, true)} />
         <QuizDeckActionsFlip />
-      </Stack>
-    </Show>
+      </Show>
+      <Show when={isDeckFinished}>
+        <QuizDeckActionsReload />
+      </Show>
+    </Stack>
   )
 }
 
