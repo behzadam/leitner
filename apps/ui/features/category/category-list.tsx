86133e0af41ecfc8106098e9a06809d8ca025d8b
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-19 15:51:19 +0330

    refactor:

--- apps/ui/features/category/category-list.tsx
+++ apps/ui/features/category/category-list.tsx
@@ -1,13 +1,8 @@
 import { Container } from '@mui/material';
-import { useMemo } from 'react';
 
 import CategoryListDataGrid from './category-list-data-grid';
 
 const CategoryList = (): JSX.Element => {
-  const categories: number[] = useMemo(() => {
-    return Array.from({ length: 10 })
-  }, [])
-
   return (
     <Container maxWidth="lg" sx={{ height: 422, mt: 4 }}>
       <CategoryListDataGrid />
