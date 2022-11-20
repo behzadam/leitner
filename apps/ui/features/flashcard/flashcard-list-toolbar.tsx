615fa180c243424ed13778ca2f5d0f6aeecd00ed
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-20 12:37:22 +0330

    refactor: remove state action component

--- apps/ui/features/flashcard/flashcard-list-toolbar.tsx
+++ apps/ui/features/flashcard/flashcard-list-toolbar.tsx
@@ -2,7 +2,6 @@ import { Stack, Typography } from "@mui/material";
 import FlashcardListActionCreate from "./flashcard-list-action-create";
 import FlashcardListActionDelete from "./flashcard-list-action-delete";
 import FlashcardListActionQuiz from "./flashcard-list-action-quiz";
-import FlashcardListActionStat from "./flashcard-list-action-stat";
 
 const FlashcardListToolbar = (): JSX.Element => {
   return (
@@ -12,7 +11,6 @@ const FlashcardListToolbar = (): JSX.Element => {
       </Typography>
       <FlashcardListActionDelete />
       <Stack direction="row" alignItems="center" spacing={1} sx={{ ml: 'auto' }}>
-        <FlashcardListActionStat />
         <FlashcardListActionQuiz />
         <FlashcardListActionCreate />
       </Stack>
