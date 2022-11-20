147e63654fa5fdfcd59532d15f5b0509b784662e
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-20 09:09:59 +0330

    feat: add toolbar component

---
+++ apps/ui/features/category/category-list-toolbar.tsx
@@ -0,0 +1,19 @@
+import { Stack, Typography } from "@mui/material";
+import CategoryListAddButton from "./category-add";
+
+
+const CategoryListToolbar = (): JSX.Element => {
+
+  return (
+    <Stack direction="row" alignItems="center" sx={{ mb: 1 }}>
+      <Typography variant="subtitle1" >
+        Flashcards
+      </Typography>
+      <Stack direction="row" alignItems="center" spacing={1} sx={{ ml: 'auto' }}>
+        <CategoryListAddButton />
+      </Stack>
+    </Stack>
+  )
+}
+
+export default CategoryListToolbar;
