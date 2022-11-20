20e4844021f8497887163e5ae7ff04f977ce0294
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-11-20 09:15:44 +0330

    feat: add toolbar

--- apps/ui/features/category/category-list.tsx
+++ apps/ui/features/category/category-list.tsx
@@ -1,10 +1,12 @@
 import { Container } from '@mui/material';
 
 import CategoryListDataGrid from './category-list-data-grid';
+import CategoryListToolbar from './category-list-toolbar';
 
 const CategoryList = (): JSX.Element => {
   return (
     <Container maxWidth="lg" sx={{ height: 422, mt: 4 }}>
+      <CategoryListToolbar />
       <CategoryListDataGrid />
     </Container>
   )
